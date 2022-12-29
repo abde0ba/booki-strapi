0245c85fb1f35a672a3c2c5390d2ffa809911b12
abde0ba <abdesoft1@gmail.com>
2022-12-29 03:28:42 -0800

    first commit

---
+++ src/api/favorite-book/controllers/favorite-book.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * favorite-book controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::favorite-book.favorite-book');
---
+++ src/api/favorite-book/routes/favorite-book.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * favorite-book router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::favorite-book.favorite-book');
